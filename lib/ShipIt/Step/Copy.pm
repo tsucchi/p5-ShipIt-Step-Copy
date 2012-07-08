24e0ec28c810b38ab87744c9c3e7319e6ee4932b
Takuya Tsuchida <tsucchi@cpan.org>
2012-07-08 11:56:18 +0900

    initial commit

---
+++ lib/ShipIt/Step/Copy.pm
@@ -0,0 +1,32 @@
+package ShipIt::Step::Copy;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+ShipIt::Step::Copy -
+
+=head1 SYNOPSIS
+
+  use ShipIt::Step::Copy;
+
+=head1 DESCRIPTION
+
+ShipIt::Step::Copy is
+
+=head1 AUTHOR
+
+Takuya Tsuchida E<lt>tsucchi {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
