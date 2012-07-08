f8d2d1ca0fea3a00f34f54fe36e4313569ab512f
Takuya Tsuchida <tsucchi@cpan.org>
2012-07-08 15:01:40 +0900

    first version

--- lib/ShipIt/Step/Copy.pm
+++ lib/ShipIt/Step/Copy.pm
@@ -1,22 +1,47 @@
 package ShipIt::Step::Copy;
+use parent qw(ShipIt::Step);
 use strict;
 use warnings;
+use File::Spec;
+use File::Basename qw();
+use File::Copy qw();
+use Carp qw();
+
 our $VERSION = '0.01';
+
+sub init {
+    my ($self, $conf) = @_;
+    $self->{copy_dir} = $conf->value('copy.dir');
+}
+
+sub run {
+    my ($self, $state) = @_;
+    my $copy_dest_dir = $self->{copy_dir};
+    $copy_dest_dir =~ s/^~/$ENV{HOME}/;
+    if ( !-d $copy_dest_dir ) {
+        mkdir $copy_dest_dir or Carp::croak("can't create directory $copy_dest_dir : $!");
+    }
+    my $new_dist_file = File::Spec->catfile($copy_dest_dir, File::Basename::basename($state->distfile));
+    File::Copy::copy($state->distfile, $new_dist_file) or Carp::croak("copy failed : $!");
+}
 
 1;
 __END__
 
 =head1 NAME
 
-ShipIt::Step::Copy -
+ShipIt::Step::Copy - provide simple copy step for module distribution to ShipIt.
 
 =head1 SYNOPSIS
 
-  use ShipIt::Step::Copy;
+  # in your .shipit
+  steps = ..., Copy
+  copy.dir = ~/Dropbox/Public/My-Module
 
 =head1 DESCRIPTION
 
-ShipIt::Step::Copy is
+ShipIt::Step::Copy provides simple copy step to ShipIt. This is useful for backup to another disk or ship to public using DropBox(this usage is shown in SYNOPSIS).
+It is useful for modules you don't want to upload CPAN.
 
 =head1 AUTHOR
 
